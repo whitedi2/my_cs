83a1f662294ebeccf5a68dfc149e1aeba6fef3cc
bdy <bdy@sab2up.ru>
2026-06-10 10:50:51 +0300

    лок файлы

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration for game paths
+# Default paths based on existing project structure
+CSTRIKE_PATH = r"D:\SteamLibrary\steamapps\common\Half-Life\cstrike"
+WAD_FILE = "cs_dust.wad"
+
+# Full path to the wad file
+WAD_PATH = Path(CSTRIKE_PATH) / WAD_FILE
